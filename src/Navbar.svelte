f0d277e6cae2b4bad8b6d4d8ec6b8663772cab04
John Henrik Muller <johnhenrikmuller@gmail.com>
2021-03-15 20:56:25 +0100

    Messing around adding some stuff.

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,29 +1,25 @@
 <!-- Bootstrap 5 Navbar. Need to decide what frameworks we're gonna use. 
 Doesn't work atm, not sure how we're gonna get bootstrap working with Svelte.-->
-
 <script>
 
 </script>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="../index.html">TI4 Companion App</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Overview</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Techs</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <a class="nav-link" href="#">Scoring</a>
           </li>
         </ul>
       </div>
